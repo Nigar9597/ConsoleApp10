6768d7058f4d0c7f71d083e3eeaf15fc96c4e358
@Nigar9597 <@nigarza-bb208@code.edu.az>
2025-03-19 23:25:03 +0400

    Add project files.

---
+++ ConsoleApp10/BankAccount.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class BankAccount
+    {
+        private decimal balance;
+        public decimal Balance { get; }
+        public string AccountNumber { get; }
+        public string OwnerName { get; set; }
+
+        public BankAccount(decimal balans)
+        {
+            
+        }
+        public void Deposit(decimal amount)
+        {if(amount<=0)
+            {
+                Console.WriteLine("mebleg musbet olmalidir");
+            }
+            else if(amount>=0)
+            {
+                balance += amount;
+                Console.WriteLine("hesaba mebleg elave edildi");
+            }
+
+            
+
+        }
+         public void WithDraw(decimal amount)
+        {
+            if(amount<=0)
+            {
+                Console.WriteLine( "cixarilan mebleg musbet olmalidir");
+                       
+             }
+            else if(amount>balance)
+            {
+                Console.WriteLine("balansda kifayet qeder mebleg yoxdur");
+            }
+
+            else
+            {
+                balance -= amount;
+                Console.WriteLine($"{amount} cixarildi, cari balans: {balance}");
+            }
+        }  
+       
+        public void DisplayAcoountInfo()
+        {
+            Console.WriteLine($"account number:{AccountNumber}");
+            Console.WriteLine($"owner name: {OwnerName}");
+            Console.WriteLine($"balance:{balance}");
+        }
+
+        public void TransferFunds(BankAccount recipient, decimal amount)
+
+        {
+            if (amount > 0 && amount <= balance)
+            {
+                balance -= amount;
+                recipient.balance+=amount;
+                Console.WriteLine($"{amount} AZN kocuruldu {recipient.OwnerName}.");
+            }
+            else
+            {
+                Console.WriteLine("pul transferi mumkun deyil.");
+            }
+        }
+    }
+}
---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Security.Principal;
+using System.Transactions;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Balans ne qeder olacaq?");
+            decimal balans = Convert.ToDecimal(Console.ReadLine());
+            BankAccount bankAccount1 = new BankAccount(balans);
+            string userAnswer = "";
+
+            while (userAnswer != "0")
+            {
+                Console.WriteLine("Hansi emeliyyati icra etmek isteyirsen?");
+                Console.WriteLine("1. Deposit\r\n  2. Withdraw\r\n  3. AccountInfo\r\n  4. Change OwnerName\r\n  5. TransferFunds\r\n  0. Quit Application");
+                userAnswer = Console.ReadLine();
+                switch (userAnswer)
+                {
+                    case "1":
+                        Console.WriteLine("Deposit elave etmek istediyiniz meblegi qeyd edin:");
+                        decimal depositAmount = Convert.ToDecimal(Console.ReadLine());
+                        bankAccount1.Deposit(depositAmount);
+                        break;
+                    case "2":
+                        Console.WriteLine("Cixarmaq istediyiniz meblegi yazin");
+                        decimal withdrawAmount = Convert.ToDecimal(Console.ReadLine());
+                        bankAccount1.WithDraw(withdrawAmount);
+                        break;
+                    case "5":
+                        Console.WriteLine("transfer etmek istediyiniz meblegi daxil edin");
+                        decimal transferfundsAmount = Convert.ToDecimal(Console.ReadLine());
+                        bankAccount1.TransferFunds(transferfundsAmount);
+                        break;
+
+                    default:
+                        Console.WriteLine("Sehv sechim etdin, yeniden sec");
+                        break;
+                }
+
+            }
+
+        }
+    }
+}
